50f5078232658ddb09764360ce0d6df059d44255
keithchasen <keithchasen89@gmail.com>
2024-09-02 21:09:58 +0200

    sun and amdient light shader for standard material

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-import * as THREE from 'three';
+import {
+    AmbientLight,
+    BoxGeometry, 
+    DirectionalLight, 
+    Mesh, 
+    MeshStandardMaterial, 
+    PerspectiveCamera, 
+    Scene, 
+    WebGLRenderer 
+} from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x003300 } );
-const cube = new THREE.Mesh( geometry, material );
+const sun = new DirectionalLight();
+sun.position.set(1,1,1);
+scene.add(sun);
+
+const ambient = new AmbientLight();
+ambient.intensity = 1;
+scene.add(ambient);
+
+const geometry = new BoxGeometry( 1, 1, 1 );
+const material = new MeshStandardMaterial( { color: 0x003300 } );
+const cube = new Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
